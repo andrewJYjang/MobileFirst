5b786e9e90df0ecc1aa49c977d0404b2e97f456f
andrewJYjang <solution.jang@gmail.com>
2019-05-24 17:45:48 +0900

    Add files via upload

---
+++ Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject wallPrefab;
+    public float interval;
+    public float range = 3.0f;
+
+	// Use this for initialization
+	IEnumerator Start ()
+    {
+		while (true)
+        {
+            transform.position = new Vector3(transform.position.x, Random.Range(-range, range),
+                transform.position.z);
+            Instantiate(wallPrefab, transform.position, transform.rotation);
+            yield return new WaitForSeconds(interval);
+        }
+	}	
+
+}
