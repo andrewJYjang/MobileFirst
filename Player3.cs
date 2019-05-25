4d7dc39491999a5b108a48568c6776d43fa70aac
andrewJYjang <solution.jang@gmail.com>
2019-05-25 18:14:45 +0900

    Add files via upload

---
+++ Player3.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player3 : MonoBehaviour {
+
+    public float jumpPower;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) == true)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, jumpPower, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        SceneManager.LoadScene(Application.loadedLevel);
+    }
+}
