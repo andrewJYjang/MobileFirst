ff8dac3cd88fccf3c737f6419970ab11d5146a06
andrewJYjang <solution.jang@gmail.com>
2019-05-25 18:14:23 +0900

    Add files via upload

---
+++ Player2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player2 : MonoBehaviour
+{
+
+    public float jumpPower;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, jumpPower, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        SceneManager.LoadScene(Application.loadedLevel);
+    }
+}
